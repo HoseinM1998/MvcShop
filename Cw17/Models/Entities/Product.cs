40c885e4b8bed12aba33cf2779d3d2c3bccb5163
HosseinM1998 <hmoslehi98@gmail.com>
2024-12-29 22:12:09 +0330

    debug

--- Cw17/Models/Entities/Product.cs
+++ Cw17/Models/Entities/Product.cs
@@ -1,8 +1,14 @@
-﻿namespace Cw17.Models.Entities
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cw17.Models.Entities
 {
     public class Product
     {
         public int Id { get; set; }
+        [DisplayName("اسم محصول")]
+        [Required]
+    
         public string Name { get; set; }
         public string Title { get; set; }
         public int Price { get; set; }
--- Cw17/obj/Debug/net9.0/Cw17.AssemblyInfo.cs
+++ Cw17/obj/Debug/net9.0/Cw17.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cw17")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2853d775c6a7357eaf2c712c81318fced06b7ac6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cw17")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cw17")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
